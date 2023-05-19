f2430af66c507b5c4658ce11ccf5154dcee468db
khosbilegt <khosookoo@gmail.com>
2023-05-20 00:47:04 +0800

    added image

---
+++ portfolio/src/components/About.js
@@ -0,0 +1,9 @@
+function About() {
+     return (
+          <div className=" bg-slate-800">
+               <p>Test</p>
+          </div>
+     )
+}
+
+export default About;
--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css';
 
 function Navbar() {
      return (
-          <div className='Navbar'>
-               <ul>
-                    <Link to="/">Test</Link>
-                    <Link to="/">Test</Link>
-                    <Link to="/">Test</Link>
-               </ul>
-          </div>
+          <nav className="flex justify-center items-center bg-gray-800 text-white h-16">
+               <div className="flex space-x-8">
+                    <Link to="/" className='text-teal-200 hover:text-teal-600'></Link>
+                    <Link to="/" className='text-teal-200 hover:text-teal-600'>About me</Link>
+                    <Link to="/" className='text-teal-200 hover:text-teal-600'>Services</Link>
+                    <Link to="/" className='text-teal-200 hover:text-teal-600'>Works</Link>
+                    <Link to="/" className='text-teal-200 hover:text-teal-600'>Contact</Link>
+               </div>
+          </nav>
      );
 }
 
