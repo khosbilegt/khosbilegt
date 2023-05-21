5f8e177c516b73437d61e7826d8be0fe6a0ba1a2
khosbilegt <khosookoo@gmail.com>
2023-05-21 23:23:51 +0800

    changed logos and favicon

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -4,7 +4,7 @@ import styles from './styles';
 
 const App = () => {
   useEffect(() => {
-    document.title = "Khosoo's Portfolio";
+    document.title = "Xocoo's Portfolio";
   }, []);
 
   return (
